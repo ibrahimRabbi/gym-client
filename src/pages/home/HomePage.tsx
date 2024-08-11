1d594a513caa2c26ea155698f1cb210e9ed89c81
ibrahim <ibrahimrabbihere@gmail.com>
2024-08-11 16:04:21 +0600

    category section has been implemeneted

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,4 @@
+import Category from "../../components/category/Category";
 import Header from "../../components/header/Header";
 
 
@@ -9,7 +10,7 @@ const HomePage = () => {
     return (
         <div>
             <Header />
-            <p>scskscskcjskjskjskjsdk Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa iure voluptatibus rerum labore voluptatum omnis consequatur eligendi harum ipsum odit! Veniam placeat beatae illo et quo qui, consequuntur quibusdam nobis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, harum quaerat error deserunt in, magni iusto corrupti nobis quia debitis doloremque, reprehenderit recusandae ex minima accusantium ducimus unde vitae eveniet. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit numquam delectus, velit quasi vel placeat tempore quaerat adipisci hic quo inventore facere tempora ullam veniam cumque eaque aut, ea quisquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi corrupti voluptate officia voluptatibus earum soluta officiis iusto, voluptates repellendus mollitia nihil vero quisquam blanditiis magnam laboriosam cumque labore hic. Quaerat excepturi, doloribus unde obcaecati atque alias labore iusto, explicabo ipsam enim, quasi quia doloremque optio.</p>
+             <Category/>
         </div>
     );
 };
---
+++ src/utils/Title.tsx
@@ -0,0 +1,9 @@
+
+const Title = ({title}:any) => {
+    return <div className="w-[30%] text-slate-300 animate-pulse duration-75">
+        <p className="text-3xl font-bold text-yellow-200 ">{title} :</p>
+        <p className="text-slate-500 mt-1">Lorem ipsum dolor sit amet consectetur adipisicing </p>
+    </div>
+};
+
+export default Title;
